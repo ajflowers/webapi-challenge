71ab65484afa63d00921fb367f10c240b3a946e7
Angela Flowers <me@angela.flowers>
2019-11-01 11:48:55 -0400

    server running

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const cors = require('cors')
+
+const actionRouter = require('./routes/actionRouter');
+// const projectRouter = require('./routes/projectRouter');
+
+const server = express();
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'server functional' })
+});
+
+server.use(express.json());
+server.use(cors());
+
+
+server.use('/api/actions/', actionRouter);
+// server.use('/api/projects/', projectRouter);
+
+module.exports = server;
